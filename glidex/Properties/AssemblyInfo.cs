f4e15a73151a29cd379e3e0f24e1531a0505fda3
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-02-26 22:09:40 -0600

    Initial binding project
    - .gitattributes
    - .gitignore
    - Wrote some MSBuild stuff to download the Jar files from Github

---
+++ glidex/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+[assembly: AssemblyTitle ("glidex")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Jonathan Peppers")]
+[assembly: AssemblyProduct ("glidex")]
+[assembly: AssemblyCopyright ("Copyright © Jonathan Peppers 2018")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: ComVisible (false)]
+[assembly: AssemblyVersion ("4.6.1.0")]
+[assembly: AssemblyFileVersion ("4.6.1.0")]
