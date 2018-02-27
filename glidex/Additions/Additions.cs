435ccad38e99a6dc0414a4e7a84b16d185486a4f
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-02-27 09:02:13 -0600

    Got past initial set of errors

---
+++ glidex/Additions/Additions.cs
@@ -0,0 +1,52 @@
+﻿namespace Com.Bumptech.Glide.Load.Data
+{
+	partial class AssetFileDescriptorLocalUriFetcher
+	{
+		protected override Java.Lang.Object LoadResource (global::Android.Net.Uri p0, global::Android.Content.ContentResolver p1)
+		{
+			return LoadFile (p0, p1);
+		}
+	}
+
+	partial class FileDescriptorAssetPathFetcher
+	{
+		protected override Java.Lang.Object LoadResource (Android.Content.Res.AssetManager p0, string p1)
+		{
+			return LoadFile (p0, p1);
+		}
+	}
+
+	partial class FileDescriptorLocalUriFetcher
+	{
+		protected override Java.Lang.Object LoadResource (global::Android.Net.Uri p0, global::Android.Content.ContentResolver p1)
+		{
+			return LoadFile (p0, p1);
+		}
+	}
+
+	partial class StreamAssetPathFetcher
+	{
+		protected override Java.Lang.Object LoadResource (Android.Content.Res.AssetManager p0, string p1)
+		{
+			var handle = Android.Runtime.InputStreamAdapter.ToLocalJniHandle (LoadFile (p0, p1));
+			try {
+				return new Java.Lang.Object (handle, Android.Runtime.JniHandleOwnership.TransferLocalRef);
+			} finally {
+				Android.Runtime.JNIEnv.DeleteLocalRef (handle);
+			}
+		}
+	}
+
+	partial class StreamLocalUriFetcher
+	{
+		protected override Java.Lang.Object LoadResource (global::Android.Net.Uri p0, global::Android.Content.ContentResolver p1)
+		{
+			var handle = Android.Runtime.InputStreamAdapter.ToLocalJniHandle (LoadFile (p0, p1));
+			try {
+				return new Java.Lang.Object (handle, Android.Runtime.JniHandleOwnership.TransferLocalRef);
+			} finally {
+				Android.Runtime.JNIEnv.DeleteLocalRef (handle);
+			}
+		}
+	}
+}
