5d23c1f45b18a979921508de8ee1296f47dff142
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-02-27 09:54:24 -0600

    Sample app, proving it works so far

---
+++ glidex.sample/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Glide;
+using Android.Glide.Request;
+
+namespace Android.Glide.Sample
+{
+	[Activity (Label = "glidex.sample", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light")]
+	public class MainActivity : AppCompatActivity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			SetContentView (Resource.Layout.Main);
+
+			var image = FindViewById<ImageView> (Resource.Id.testImage);
+			Glide.With (this)
+				.Load ("https://botlist.co/system/BotList/Bot/logos/000/002/271/medium/chuck_norris.jpg")
+				.Apply (RequestOptions.CircleCropTransform ())
+				.Into (image);
+		}
+	}
+}
+
---
+++ glidex.sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+[assembly: AssemblyTitle ("glidex.sample")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("glidex.sample")]
+[assembly: AssemblyCopyright ("Copyright ©  2018")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: ComVisible (false)]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
