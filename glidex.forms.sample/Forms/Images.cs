5337246b2c30b02c4cc2911d46a5b223139859c9
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-02-28 21:06:09 -0600

    Moved images to their own class added Resources

---
+++ glidex.forms.sample/Forms/Images.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Android.Glide.Sample
+{
+	public static class Images
+	{
+		static readonly Random random = new Random ();
+		const int MaxImages = 7;
+
+		public static IEnumerable<ImageSource> RandomSources ()
+		{
+			var random = new Random ();
+			for (int i = 0; i < 100; i++) {
+				yield return RandomSource ();
+			}
+		}
+
+		public static ImageSource RandomSource ()
+		{
+			int x = random.Next (MaxImages);
+			switch (x) {
+				case 0:
+					return ImageSource.FromUri (new Uri ("https://botlist.co/system/BotList/Bot/logos/000/002/271/medium/chuck_norris.jpg"));
+				case 1:
+					return ImageSource.FromUri (new Uri ("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Steven_Seagal_November_2016.jpg/230px-Steven_Seagal_November_2016.jpg"));
+				case 2:
+					return ImageSource.FromUri (new Uri ("https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTIwNjA4NjM0MDQyNzQ2Mzgw/hulk-hogan-9542305-1-402.jpg"));
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+					return ImageSource.FromFile ("patch" + (x - 2));
+				default:
+					throw new NotImplementedException ($"Whoops {x} not implemented!");
+			}
+		}
+	}
+}
--- glidex.forms.sample/Forms/MainPage.xaml.cs
+++ glidex.forms.sample/Forms/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace Android.Glide.Sample
@@ -25,7 +24,7 @@ namespace Android.Glide.Sample
 
 				for (int j = 0; j < 4; j++) {
 					var image = new Image {
-						Source = RandomSource (random.Next (MaxImages))
+						Source = Images.RandomSource (),
 					};
 					Grid.SetRow (image, i);
 					Grid.SetColumn (image, j);
@@ -33,30 +32,6 @@ namespace Android.Glide.Sample
 				}
 			}
 			_scroll.Content = grid;
-		}
-
-		const int MaxImages = 3;
-
-		IEnumerable<ImageSource> RandomSources ()
-		{
-			var random = new Random ();
-			for (int i = 0; i < 100; i++) {
-				yield return RandomSource (random.Next (MaxImages));
-			}
-		}
-
-		ImageSource RandomSource (int x)
-		{
-			switch (x) {
-				case 0:
-					return ImageSource.FromUri (new Uri ("https://botlist.co/system/BotList/Bot/logos/000/002/271/medium/chuck_norris.jpg"));
-				case 1:
-					return ImageSource.FromUri (new Uri ("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Steven_Seagal_November_2016.jpg/230px-Steven_Seagal_November_2016.jpg"));
-				case 2:
-					return ImageSource.FromUri (new Uri ("https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTIwNjA4NjM0MDQyNzQ2Mzgw/hulk-hogan-9542305-1-402.jpg"));
-				default:
-					throw new ArgumentNullException ($"Not implemented {x}!");
-			}
 		}
 	}
 }
