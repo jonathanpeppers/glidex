1ab2b92b57b5b8bfa5bc0874e93b83e94e724b73
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-02-28 22:39:47 -0600

    Implemented tests for file path & stream

--- glidex.forms.sample/Forms/Images.cs
+++ glidex.forms.sample/Forms/Images.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Xamarin.Forms;
 
 namespace Android.Glide.Sample
@@ -7,7 +8,9 @@ namespace Android.Glide.Sample
 	public static class Images
 	{
 		static readonly Random random = new Random ();
-		const int MaxImages = 7;
+		static readonly string tempDir = Path.Combine (Path.GetTempPath (), "glide.forms.sample");
+		static readonly string tempPath = Path.Combine (tempDir, "temp.jpg");
+		const int MaxImages = 9;
 
 		public static IEnumerable<ImageSource> RandomSources ()
 		{
@@ -21,17 +24,32 @@ namespace Android.Glide.Sample
 		{
 			int x = random.Next (MaxImages);
 			switch (x) {
+				//Urls
 				case 0:
 					return ImageSource.FromUri (new Uri ("https://botlist.co/system/BotList/Bot/logos/000/002/271/medium/chuck_norris.jpg"));
 				case 1:
 					return ImageSource.FromUri (new Uri ("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Steven_Seagal_November_2016.jpg/230px-Steven_Seagal_November_2016.jpg"));
 				case 2:
 					return ImageSource.FromUri (new Uri ("https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTIwNjA4NjM0MDQyNzQ2Mzgw/hulk-hogan-9542305-1-402.jpg"));
+				//Resources
 				case 3:
 				case 4:
 				case 5:
 				case 6:
 					return ImageSource.FromFile ("patch" + (x - 2));
+				//File path
+				case 7:
+					if (!File.Exists(tempPath)) {
+						Directory.CreateDirectory (tempDir);
+						using (var assetStream = Android.App.Application.Context.Assets.Open("assetpatch1.jpg"))
+						using (var fileStream = File.Create (tempPath)) {
+							assetStream.CopyTo (fileStream);
+						}
+					}
+					return ImageSource.FromFile (tempPath);
+				//Stream
+				case 8:
+					return ImageSource.FromStream (() => Android.App.Application.Context.Assets.Open ("assetpatch2.jpg"));
 				default:
 					throw new NotImplementedException ($"Whoops {x} not implemented!");
 			}
