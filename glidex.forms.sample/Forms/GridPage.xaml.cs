79b387eb945797806f57376ce2e09178adb96f91
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-06-20 16:26:50 -0500

    GridPage - code to demo a gesture

--- glidex.forms.sample/Forms/GridPage.xaml.cs
+++ glidex.forms.sample/Forms/GridPage.xaml.cs
@@ -4,9 +4,16 @@ namespace Android.Glide.Sample
 {
 	public partial class GridPage
 	{
+		TapGestureRecognizer gesture;
+
 		public GridPage ()
 		{
 			InitializeComponent ();
+
+			gesture = new TapGestureRecognizer ();
+			gesture.Command = new Command (async () => {
+				await DisplayAlert ("Hey!", "Gestures work, that's great!", "Ok");
+			});
 
 			for (int i = 0; i < 100; i++) {
 				_grid.RowDefinitions.Add (new RowDefinition { Height = 50 });
@@ -15,6 +22,7 @@ namespace Android.Glide.Sample
 					var image = new Image {
 						Source = Images.RandomSource (),
 					};
+					image.GestureRecognizers.Add (gesture);
 					Grid.SetRow (image, i);
 					Grid.SetColumn (image, j);
 					_grid.Children.Add (image);
