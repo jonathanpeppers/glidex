e5b44e1da49d9324ccce113246cec7534e8cafe1
Jonathan Peppers <jonathan.peppers@gmail.com>
2018-11-27 17:24:46 -0600

    Support for new ImageButton in Xamarin.Forms 3.4.x (#18)
    Fixes: https://github.com/jonathanpeppers/glidex/issues/16
    
    Apparently, Glide uses `Tag` internally. So Xamarin.Forms can't set `Tag`!
    
    To workaround this, I added a quick custom renderer to null out `Tag`. This is a workaround until a release of Xamarin.Forms with this change:
    
    https://github.com/xamarin/Xamarin.Forms/pull/4542
    
    Unfortunately, this also requires glidex.forms to bump its minimum dependency on Xamarin.Forms.

---
+++ glidex.forms/ImageButtonRenderer.cs
@@ -0,0 +1,17 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer (typeof (ImageButton), typeof (Android.Glide.ImageButtonRenderer))]
+
+namespace Android.Glide
+{
+	public class ImageButtonRenderer : Xamarin.Forms.Platform.Android.ImageButtonRenderer
+	{
+		public ImageButtonRenderer (Context context) : base (context)
+		{
+			//HACK: workaround until https://github.com/xamarin/Xamarin.Forms/pull/4542 is released
+			// See https://github.com/jonathanpeppers/glidex/issues/16
+			Tag = null;
+		}
+	}
+}
