f5ba62e38a5ed818ee43bbd6c7920a711e51ba69
Matthew Richardson <matthew@velocitysystems.co.nz>
2020-07-14 09:37:09 +0100

    Use random images from picsum API.

--- glidex.forms.sample/Forms/CollectionViewOfDoom.xaml.cs
+++ glidex.forms.sample/Forms/CollectionViewOfDoom.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Android.Glide.Sample
@@ -15,12 +16,14 @@ namespace Android.Glide.Sample
 		}
 
 		readonly Random random = new Random ();
-		const string Url = "https://img.resized.co/irishpostcouk/eyJkYXRhIjoie1widXJsXCI6XCJodHRwczpcXFwvXFxcL3MzLWV1LXdlc3QtMS5hbWF6b25hd3MuY29tXFxcL3N0b3JhZ2UucHVibGlzaGVycGx1cy5pZVxcXC9tZWRpYS5pcmlzaHBvc3QuY28udWtcXFwvdXBsb2Fkc1xcXC8yMDE5XFxcLzA3XFxcLzA0MTE0OTQ5XFxcL0RvbmFsZC1UcnVtcC1XV0UtSXJpc2gtUG9zdC5wbmdcIixcIndpZHRoXCI6NzAwLFwiaGVpZ2h0XCI6MzcwLFwiZGVmYXVsdFwiOlwiaHR0cHM6XFxcL1xcXC93d3cuaXJpc2hwb3N0LmNvbVxcXC9pXFxcL25vLWltYWdlLnBuZ1wifSIsImhhc2giOiJjMTAzMTQ1NDk1YzY2NGMwM2ZlMTc2ZmY5MzI1YmY4NTc4MWVkOGFiIn0=/donald-trump-wwe-irish-post.png?rand=";
 
 		IEnumerable<ImageSource> GetImageUrls (int count)
 		{
+			var width = (int)(DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density);
+			var url = $"https://picsum.photos/{width}/200";
+
 			for (int i = 0; i < count; i++) {
-				yield return ImageSource.FromUri (new Uri (Url + random.Next ()));
+				yield return ImageSource.FromUri (new Uri ($"{url}?{random.Next()}"));
 			}
 		}
 	}
