a70497d5878597ef8f59675f5ec515142a01d8c6
Matthew Richardson <matthew@velocitysystems.co.nz>
2020-07-14 16:23:10 +0100

    Add CollectionView of Doom sample (#68)
    Add `CollectionView` sample using dynamically loaded images from [Lorem Picsum][0].
    
    Images are dynamically requested based on device width in current orientation.
    
    [0]: https://picsum.photos

---
+++ glidex.forms.sample/Forms/CollectionViewOfDoom.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Android.Glide.Sample
+{
+	public partial class CollectionViewOfDoom : ContentPage
+	{
+		public CollectionViewOfDoom ()
+		{
+			InitializeComponent ();
+
+			BindingContext = GetImageUrls (100).ToArray ();
+		}
+
+		readonly Random random = new Random ();
+
+		IEnumerable<ImageSource> GetImageUrls (int count)
+		{
+			var width = (int)(DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density);
+			var url = $"https://picsum.photos/{width}/200";
+
+			for (int i = 0; i < count; i++) {
+				yield return ImageSource.FromUri (new Uri ($"{url}?{random.Next()}"));
+			}
+		}
+	}
+}
