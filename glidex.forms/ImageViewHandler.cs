0f3b9740b6ead95f09f715787710789f76c7712e
Yaser Moradi <ysmoradi@outlook.com>
2020-08-04 14:24:35 +0430

    Fixed small issue

--- glidex.forms/ImageViewHandler.cs
+++ glidex.forms/ImageViewHandler.cs
@@ -7,6 +7,10 @@ using Android.Widget;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
+[assembly: ExportImageSourceHandler (typeof (FileImageSource), typeof (Android.Glide.ImageViewHandler))]
+[assembly: ExportImageSourceHandler (typeof (StreamImageSource), typeof (Android.Glide.ImageViewHandler))]
+[assembly: ExportImageSourceHandler (typeof (UriImageSource), typeof (Android.Glide.ImageViewHandler))]
+
 namespace Android.Glide
 {
 	[Preserve (AllMembers = true)]
