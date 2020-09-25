b271fd4b9fd1954ea7c8cd1cd8e481057bd5580a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-09-25 14:05:55 -0500

    Samples cleanup
    It was getting a big unwieldy to add new pages. I cleaned this up so a
    single `List<T>` defines all the pages.
    
    New samples should be able to just add a page and add a single line at
    the top of `MainPage` such as:
    
        ( "My Example", () => new MyExamplePage () ),

--- glidex.forms.sample/Forms/MainPage.xaml.cs
+++ glidex.forms.sample/Forms/MainPage.xaml.cs
@@ -1,57 +1,44 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
 
 namespace Android.Glide.Sample
 {
 	public partial class MainPage : ContentPage
 	{
+		readonly List<ValueTuple<string, Func<Page>>> pages = new List<ValueTuple<string, Func<Page>>> {
+			( "Grid Example", () => new GridPage () ),
+			( "Edge Cases", () => new EdgeCasesPage () ),
+			( "ViewCells", () => new ViewCellPage () ),
+			( "ImageCells", () => new ImageCellPage () ),
+			( "Huge Images", () => new HugeImagePage () ),
+			( "Toggle Images", () => new ToggleSourcePage () ),
+			( "Toggle Images Material", () => new ToggleSourcePage { Visual = VisualMarker.Material } ),
+			( "Images Should Match", () => new MatchPage () ),
+			( "ListView Of Doom", () => new ListViewOfDoom () ),
+			( "CollectionView Of Doom", () => new CollectionViewOfDoom () ),
+			( "FlexLayout Image Buttons", () => new FlexLayoutImageButtonPage () ),
+			( "VisualStateManager", () => new VisualStateManagerPage () ),
+		};
+
 		public MainPage ()
 		{
 			InitializeComponent ();
+
+			BindingContext = pages.Select (p => p.Item1).ToArray ();
 		}
 
 		async void OnItemSelected (object sender, SelectedItemChangedEventArgs e)
 		{
-			switch (e.SelectedItemIndex) {
-				case 0:
-					await Navigation.PushAsync (new GridPage ());
-					break;
-				case 1:
-					await Navigation.PushAsync (new EdgeCasesPage ());
-					break;
-				case 2:
-					await Navigation.PushAsync (new ViewCellPage ());
-					break;
-				case 3:
-					await Navigation.PushAsync (new ImageCellPage ());
-					break;
-				case 4:
-					await Navigation.PushAsync (new HugeImagePage ());
-					break;
-				case 5:
-					await Navigation.PushAsync (new ToggleSourcePage ());
-					break;
-				case 6:
-					await Navigation.PushAsync (new ToggleSourcePage { Visual = VisualMarker.Material });
-					break;
-				case 7:
-					await Navigation.PushAsync (new MatchPage ());
-					break;
-				case 8:
-					await Navigation.PushAsync (new ListViewOfDoom ());
-					break;
-				case 9:
-					await Navigation.PushAsync (new CollectionViewOfDoom ());
-					break;
-				case 10:
-					await Navigation.PushAsync (new FlexLayoutImageButtonPage ());
-					break;
-				case 11:
-					await Navigation.PushAsync (new VisualStateManagerPage ());
-					break;
-				default:
-					break;
+			if (0 <= e.SelectedItemIndex && e.SelectedItemIndex < pages.Count) {
+				var (title, callback) = pages [e.SelectedItemIndex];
+				var page = callback ();
+				if (string.IsNullOrEmpty (page.Title))
+					page.Title = title;
+				await Navigation.PushAsync (page);
+				((ListView) sender).SelectedItem = null;
 			}
-			((ListView)sender).SelectedItem = null;
 		}
 	}
 }
