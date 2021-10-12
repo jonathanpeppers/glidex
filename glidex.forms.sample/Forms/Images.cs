fddbc8dfefb7bd3af813ccd68727508a5a48e7bb
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-10-12 08:31:24 -0500

    Use a different "Steven Seagal" image URL

--- glidex.forms.sample/Forms/Images.cs
+++ glidex.forms.sample/Forms/Images.cs
@@ -32,7 +32,7 @@ namespace Android.Glide.Sample
 				case 0:
 					return ImageSource.FromUri (new Uri ("https://growtix-melupufoagt.stackpathdns.com/media/big//34/28/23/5a9f03c2-3734-4486-bd04-2d45ac1c102e.png"));
 				case 1:
-					return ImageSource.FromUri (new Uri ("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Steven_Seagal_November_2016.jpg/230px-Steven_Seagal_November_2016.jpg"));
+					return ImageSource.FromUri (new Uri ("https://www.biography.com/.image/t_share/MTgwNTA0NTc3NjY0MDk5ODAx/gettyimages-1128793071-copy.jpg"));
 				case 2:
 					return ImageSource.FromUri (new Uri ("https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTIwNjA4NjM0MDQyNzQ2Mzgw/hulk-hogan-9542305-1-402.jpg"));
 				//Resources
